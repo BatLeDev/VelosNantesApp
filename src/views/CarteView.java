21faab63d9ead5d9fc7e1adf4fd808995d6d565e
BatLeDev <baptiste.guerny@gmail.com>
2023-05-18 21:34:49 +0200

    Séparation du html et du css du controller

--- src/views/CarteView.java
+++ src/views/CarteView.java
@@ -4,14 +4,21 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebView;
 import javafx.scene.web.WebEngine;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javafx.concurrent.Worker;
+
 /**
  * This class represents the view of the Carte page.
  * This page is a map, with a legend and sensors points.
  */
 public class CarteView extends BorderPane {
+    private static final String CARTE_HTML_PATH = "src/ressources/html/carte.html";
 
     // Elements of the view
-
+    private WebEngine webEngine;
 
     /**
      * Constructor
@@ -20,53 +27,42 @@ public class CarteView extends BorderPane {
      * Initialise the elements of the view
      */
     public CarteView() {
-
-        String htmlContent = "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "    <title>Carte sans API Google</title>\n" +
-                "    <style>\n" +
-                "        body, html {\n" +
-                "            height: 100%;\n" +
-                "            margin: 0;\n" +
-                "            padding: 0;\n" +
-                "        }\n" +
-                "\n" +
-                "        #map {\n" +
-                "            height: 100%;\n" +
-                "        }\n" +
-                "    </style>\n" +
-                "    <!-- Inclure les fichiers CSS et JavaScript de Leaflet -->\n" +
-                "    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css\" />\n"
-                +
-                "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js\"></script>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "    <div id=\"map\"></div>\n" +
-                "\n" +
-                "    <script>\n" +
-                "        // Initialiser la carte\n" +
-                "        var map = L.map('map').setView([47.2184, -1.5536], 13);\n" +
-                "\n" +
-                "        // Ajouter une couche de tuiles OpenStreetMap\n" +
-                "        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\n" +
-                "            attribution: '© OpenStreetMap contributors'\n" +
-                "        }).addTo(map);\n" +
-                "\n" +
-                "        // Ajuster la taille de la carte lorsque la fenêtre est redimensionnée\n" +
-                "        map.on('resize', function () {\n" +
-                "            map.invalidateSize();\n" +
-                "        });\n" +
-                "    </script>\n" +
-                "</body>\n" +
-                "</html>";
-
         // Create the map view
         WebView webView = new WebView();
-        WebEngine webEngine = webView.getEngine();
-        webEngine.loadContent(htmlContent);
+        this.webEngine = webView.getEngine();
+
+        this.webEngine.loadContent(readHTMLFile(CARTE_HTML_PATH));
+
+        // Load the markers when the page is loaded
+        this.webEngine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == Worker.State.SUCCEEDED) {
+                loadMarkers();
+            }
+        });
 
         // Positioning of the elements
         setCenter(webView);
+    }
+
+    private void loadMarkers() {
+        String js = "var marker = L.marker([47.2184, -1.5536]).addTo(map);\n" +
+                "marker.bindPopup(\"<b>Hello world!</b><br>I am a popup.\").openPopup();";
+
+        this.webEngine.executeScript(js);
+    }
+
+    public String readHTMLFile(String filePath) {
+        StringBuilder content = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return content.toString();
     }
 }
