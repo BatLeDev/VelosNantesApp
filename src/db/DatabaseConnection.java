204a611abf95a34fb041c7305e099e92252256b7
Corentin Piton <corentin.piton31@gmail.com>
2023-05-21 17:56:20 +0200

    utilisateur modifé (read-only)

--- src/db/DatabaseConnection.java
+++ src/db/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String DB_URL = "jdbc:mysql://31.39.13.59:6543/velosNantes";
-    private static final String DB_USER = "devs";
-    private static final String DB_PASSWORD = "oulalaTuN.aPaslemotdepasse";
+    private static final String DB_USER = "lecteur";
+    private static final String DB_PASSWORD = "Est-ce que c'est bon pour vous ?";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
