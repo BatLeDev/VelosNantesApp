420d27ebe8edb44baf26305a615b3f1fc79c6071
BatLeDev <baptiste.guerny@gmail.com>
2023-05-21 18:15:37 +0200

    Convert to static

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,11 +1,55 @@
-import db.DatabaseAccess;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class MainApp {
+// Controllers import
+import controllers.Rooter;
+import controllers.ExporterController;
+import controllers.CarteController;
+import controllers.NavbarController;
 
+/**
+ * Main class
+ * Run this class to launch the application
+ * 
+ * This class extends Application from JavaFX
+ */
+public class MainApp extends Application {
+
+    /**
+     * Rooter instance
+     * Used to change the page displayed
+     */
+    private Rooter rooter;
+
+    /**
+     * Main method
+     * 
+     * @param args isn't used
+     */
     public static void main(String[] args) {
-        DatabaseAccess databaseAccess = new DatabaseAccess();
-        databaseAccess.exemple();
-        
+        launch(args); // Start the application
     }
 
+    /**
+     * Start method
+     * This method is called by the launch method
+     * 
+     * @param primaryStage Stage instance (main window, generate by JavaFX)
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        // Creation of the rooter
+        rooter = new Rooter(primaryStage);
+
+        // Initialisation of each controller
+        // Isn't necessary to keep the reference of the controller
+        new ExporterController(rooter);
+        new CarteController(rooter);
+
+        // Initialisation of the navbar controller
+        new NavbarController(rooter);
+
+        // Show the main page (Carte)
+        rooter.changePage(true, "Carte");
+    }
 }
